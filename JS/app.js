b2e26b78affb5b946066366bb4c473f27cbdbc8e
AliMwas <alimwas61@gmail.com>
2021-08-02 15:15:47 +0300

    create about me

---
+++ JS/app.js
@@ -0,0 +1,87 @@
+'use strict';
+
+
+
+ let name = prompt('Tell me whats your name please');
+
+ console.log(name);
+
+ alert("Welcome! in our website"+"  " +name);
+
+ let Q1 = prompt('Is the Mercedes is my favorite car (yes/no)?');
+ Q1 = Q1.toLowerCase();
+
+if (Q1 == 'yes'){
+
+    //console.log('you are right.')
+    alert('you are right.');
+
+    
+}
+else{
+
+    //console.log('you are wrong.')
+    alert ('you are wrong.')
+}
+
+ let Q2 = prompt('Is red my favorite color (yes/no)?');
+ Q2 = Q2.toLowerCase();
+
+ if (Q2 == 'yes'){
+
+     //console.log('you are wrong.')
+    alert('you are wrong.');
+}
+else{
+
+    //console.log('you are right.')
+    alert ('you are right.')
+}
+
+ let Q3 = prompt('Did you think I like Pizza (yes/no)?');
+ Q3 = Q3.toLowerCase();
+
+ if (Q3 == 'yes'){
+
+    //console.log('you are right.')
+    alert('you are right.');
+    
+}
+else{
+    //console.log('you are wrong.')
+    alert ('you are wrong.')
+}
+
+ let Q4 = prompt('Did you think I like programming (yes/no)?');
+ Q4 = Q4.toLowerCase();
+
+ if (Q4 == 'yes'){
+
+    //console.log('you are right.')
+    alert('you are right.');
+}
+else{
+
+    //console.log('you are wrong.')
+    alert ('you are wrong.')
+}
+
+ let Q5 = prompt('Did you think Jordan is my favorite country (yes/no)?');
+ Q5 = Q5.toLowerCase();
+ 
+ if (Q5 == 'yes'){
+
+    //console.log('you are wrong.')
+    alert('you are wrong.');
+}
+else{
+
+    //console.log('you are right.')
+    alert ('you are right.')
+}
+
+alert("Thanks for your time "+"  " +name);
+
+
+ 
+
