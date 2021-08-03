caccb239f15e2ce66a56c3a883d55884c51271c1
AliMwas <alimwas61@gmail.com>
2021-08-03 21:40:44 +0300

    lab3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+let counter = 0;
 
 
  let name = prompt('Tell me whats your name please');
@@ -10,18 +11,24 @@
 
  let Q1 = prompt('Is the Mercedes is my favorite car (yes/no)?');
  Q1 = Q1.toLowerCase();
-
+ 
 if (Q1 == 'yes'){
 
     //console.log('you are right.')
     alert('you are right.');
 
-    
+    counter+=1;
 }
-else{
+else if(Q1 == 'no'){
 
     //console.log('you are wrong.')
     alert ('you are wrong.')
+}
+else {
+alert("please enter yes or no")
+
+
+
 }
 
  let Q2 = prompt('Is red my favorite color (yes/no)?');
@@ -31,13 +38,20 @@ else{
 
      //console.log('you are wrong.')
     alert('you are wrong.');
+    
 }
-else{
+else if (Q2 == 'no'){
 
     //console.log('you are right.')
     alert ('you are right.')
+
+    counter+=1;
 }
 
+else {
+    alert("please enter yes or no")
+  
+}
  let Q3 = prompt('Did you think I like Pizza (yes/no)?');
  Q3 = Q3.toLowerCase();
 
@@ -45,11 +59,17 @@ else{
 
     //console.log('you are right.')
     alert('you are right.');
+    counter+=1;
     
 }
-else{
+else if (Q3 == 'no') {
     //console.log('you are wrong.')
     alert ('you are wrong.')
+}
+
+else {
+    alert("please enter yes or no")
+ 
 }
 
  let Q4 = prompt('Did you think I like programming (yes/no)?');
@@ -59,11 +79,17 @@ else{
 
     //console.log('you are right.')
     alert('you are right.');
+    counter+=1;
 }
-else{
+else if (Q4 == 'no') {
 
     //console.log('you are wrong.')
     alert ('you are wrong.')
+}
+
+else {
+    alert("please enter yes or no")
+
 }
 
  let Q5 = prompt('Did you think Jordan is my favorite country (yes/no)?');
@@ -74,14 +100,88 @@ else{
     //console.log('you are wrong.')
     alert('you are wrong.');
 }
-else{
+else if (Q5 == 'no'){
 
     //console.log('you are right.')
     alert ('you are right.')
+    counter+=1;
+}
+else{
+    alert("please enter yes or no")
+   
+
 }
 
-alert("Thanks for your time "+"  " +name);
+
+let weight = Number(prompt("guess my weight?"))
+
+if(weight == 110){
+    counter+=1;
+    alert("right im " + weight )
+}
+
+else {
+    for(let i = 0; i < 3; i++){
+
+        if(weight > 110)
+    alert("weight is too high" )
+
+    if(weight < 110)
+    alert("weight is too low" )
+
+    weight = Number(prompt("What is my weight? , the choices are 100, 110, 120"))
+
+    if(weight == 110){
+        counter+=1;
+    alert("right im " + weight )
+    break;}
+    }
+    if(weight != 110)
+    alert("the right answer for my weight is 110" ) 
+}
 
 
+
+
+
+
+
+
+let FavoriteFruit = prompt("guess my favorite fruit you can choose one of these options(apple / orange / watermelon / strawberry / peach / banana)")
+
+FavoriteFruit = FavoriteFruit.toLowerCase();
+
+let myFavoriteFruit = ["apple", "strawberry", "watermelon"];
+
+let boolean = false;
+
+for(let j = 0; j < 5; j++){
+
+for(let i = 0; i < myFavoriteFruit.length; i++){
+
+ if(FavoriteFruit == myFavoriteFruit[i]){
+    alert("thats a right answer" ) 
+    boolean = true;
+    break;
+ }
+}
+
+if(boolean == false){
+    alert("thats not right try again" ) 
+    FavoriteFruit = prompt("guess my favorite fruit you can choose one of these options(apple / orange / watermelon / strawberry / peach / banana)")
+    FavoriteFruit = FavoriteFruit.toLowerCase()
+}
+if(boolean == true){
+    counter+=1;
+    break;
+}
+
+}
+if(boolean == false){
+    alert("the right answer is apple, strawberry, watermelon" )
+
+}
+
+alert("Thanks for your time "+"  " +name+" your score from 7 is "+ counter );
  
 
